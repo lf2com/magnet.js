14f1d3514011a670f59b5ffd21582ef6a8f753d9
Wan Wan <lf2com.bt@gmail.com>
2022-01-28 23:34:41 +0800

    Remove unused options of judgeAttraction

--- src/methods/static/judgeAttraction.ts
+++ src/methods/static/judgeAttraction.ts
@@ -1,10 +1,6 @@
 import Magnet from '../..';
 import { SingleAttraction } from './calcSingleAttraction';
 
-export interface JudgeAttractionOptions {
-  onXYAxes?: boolean;
-}
-
 /**
  * Returns true if the attraction passes the judgement. Otherwise the
  * attraction would not be on the result list of attractions.
@@ -12,18 +8,8 @@ export interface JudgeAttractionOptions {
 function judgeAttraction(
   this: Magnet | void,
   attraction: SingleAttraction,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  options?: JudgeAttractionOptions | Magnet,
 ): boolean {
-  const standOptinos = (options ?? {}) as JudgeAttractionOptions;
-  const {
-    onXYAxes = false,
-  } = standOptinos;
-
-  return Boolean((onXYAxes
-    ? attraction.best.x && attraction.best.y
-    : attraction.best.x ?? attraction.best.y
-  ));
+  return Boolean(attraction.best.x ?? attraction.best.y);
 }
 
 export type OnJudgeAttraction = typeof judgeAttraction;
